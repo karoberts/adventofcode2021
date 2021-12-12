cf3d85f469df48005eab0877a517b915c30b040f
kroberts <cppwriter@yahoo.com>
2021-12-11 22:24:55 -0800

    12-2

--- 12.py
+++ 12.py
@@ -30,3 +30,38 @@ visited = set()
 visited.add(cur)
 paths = count_paths(cur, cxns, visited)
 print('part1', paths)
+
+def count_paths2(node, cs, visited, little2cave, little2cave_ct, curpath, uniq):
+    paths = 0
+    for next in cs[node]:
+        if next == 'start':
+            continue
+        if next == 'end':
+            pathstr = ','.join(curpath + ['end'])
+            if pathstr not in uniq:
+                #print(littlecave, little2cave_ct, pathstr)
+                paths += 1
+                uniq.add(pathstr)
+            continue
+        if next == little2cave:
+            if little2cave_ct >= 2:
+                continue
+        elif not next.isupper() and next in visited:
+            continue
+
+        n_visited = set(visited)
+        n_visited.add(next)
+        n_cp = list(curpath)
+        n_cp.append(next)
+        paths += count_paths2(next, cs, n_visited, little2cave, little2cave_ct + (1 if next == little2cave else 0), n_cp, uniq)
+
+    return paths
+
+paths = 0
+uniq = set()
+for littlecave in (c for c in cxns.keys() if not c.isupper() and c != 'start' and c != 'end'):
+    cur = 'start'
+    visited = set()
+    visited.add(cur)
+    paths += count_paths2(cur, cxns, visited, littlecave, 0, ['start'], uniq)
+print('part2', paths)
