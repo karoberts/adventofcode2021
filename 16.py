b191c9a597bbf3518b9ab6a30efffa39bdbd3e4b
kroberts <cppwriter@yahoo.com>
2021-12-17 20:42:40 -0800

    16-2

--- 16.py
+++ 16.py
@@ -1,9 +1,15 @@
+from typing import ForwardRef
+from math import prod
+import json
+
+
 with open("16.txt") as f:
     hexcodes = f.readline().strip()
 #hexcodes = '620080001611562C8802118E34'
 #hexcodes = 'C0015000016115A2E0802F182340'
 #hexcodes = 'A0016C880162017C3686B18A3D4780'
 #hexcodes = '805311100469800804A3E488ACC0B10055D8009548874F65665AD42F60'
+#hexcodes = '9C005AC2F8F0'
 
 bits = ''.join(bin(t)[2:].rjust(8, '0') for t in bytearray.fromhex(hexcodes))
 
@@ -21,15 +27,17 @@ def parse_literal(bits, pos):
             #   pos += 4 - (pos % 4)
             return (int(num, 2), pos)
 
-def parse_operator(bits, pos, versions, depth):
+def parse_operator(bits, pos, versions, op, depth):
     def parse_op(bits, pos, totpackets, totbits):
+        subops = []
         if totpackets is not None:
             #print(' ' * depth, 'parsing', totpackets, 'subpackets')
             for packet in range(0, totpackets):
-                pos = parse(bits, pos, versions, depth + 1)
+                pos = parse(bits, pos, versions, subops, True, depth + 1)
         elif totbits is not None:
             #print(' ' * depth, 'parsing', totbits, 'subpacket bits')
-            pos = parse(bits[:pos + totbits], pos, versions, depth + 1)
+            pos = parse(bits[:pos + totbits], pos, versions, subops, False, depth + 1)
+        op['sub'] = subops
         return pos
 
     i = bits[pos]
@@ -46,7 +54,7 @@ def parse_operator(bits, pos, versions, depth):
         pos += 11
         return parse_op(bits, pos, npackets, None)
 
-def parse(bits, pos, versions, depth):
+def parse(bits, pos, versions, ops, single, depth):
 
     while pos < len(bits):
         #print(' ' * depth, bits[pos:])
@@ -67,15 +75,49 @@ def parse(bits, pos, versions, depth):
         if typ == 4: # literal
             #print(' ' * depth, '=> literal')
             (literal, pos) = parse_literal(bits, pos)
+            ops.append({'t': 'Literal', 'val': literal})
             #print('got', literal)
         else: # operator
             #print(' ' * depth, '=> operator')
-            pos = parse_operator(bits, pos, versions, depth + 1)
+            op = {'t': 'Operator', 'o': typ, 'sub': []}
+            ops.append(op)
+            pos = parse_operator(bits, pos, versions, op, depth + 1)
+        
+        if single:
+            break
 
     return pos
 
 #print(bits)
 
 versions = []
-parse(bits, 0, versions, 0)
+ops = []
+parse(bits, 0, versions, ops, False, 0)
 print('part1', sum(versions))
+
+#print(json.dumps(ops, indent=None))
+
+def recurse(op):
+    if op['t'] == 'Literal':
+        return op['val']
+    
+    if op['o'] == 0:
+        return sum( (recurse(x) for x in op['sub']) )
+    if op['o'] == 1:
+        return prod( (recurse(x) for x in op['sub']) )
+    if op['o'] == 2:
+        return min( (recurse(x) for x in op['sub']) )
+    if op['o'] == 3:
+        return max( (recurse(x) for x in op['sub']) )
+    v1 = recurse(op['sub'][0])
+    v2 = recurse(op['sub'][1])
+    if op['o'] == 5:
+        return 1 if v1 > v2 else 0
+    if op['o'] == 6:
+        return 1 if v1 < v2 else 0
+    if op['o'] == 7:
+        return 1 if v1 == v2 else 0
+
+print('part2', recurse(ops[0]))
+
+
