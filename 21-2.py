2dd61ac4ec6903df7e0f4ce42841aca78d8b3453
kroberts <cppwriter@yahoo.com>
2022-01-02 19:41:46 -0800

    down to 1m55s

--- 21-2.py
+++ 21-2.py
@@ -18,13 +18,17 @@ memo = dict()
 seq1 = [p1]
 seq2 = [p2]
 
-def find(seq1, seq2, rolls, p1):
+dice_rolls = {6: 7, 5: 6, 7: 6, 4: 3, 8: 3, 3: 1, 9: 1}
+
+def find(seq1, seq2, roll, p1):
+    global dice_rolls
+
     def nx(c, r):
         return (c + r - 1) % 10 + 1
 
     next_turn = p1
 
-    if len(rolls) == 0:
+    if roll < 0:
         k = str(seq1) + '|' + str(seq2)
         if k in memo:
             return memo[k]
@@ -33,33 +37,37 @@ def find(seq1, seq2, rolls, p1):
             return (1, 0)
         if sum(seq2) - seq2[0] >= 21:
             return (0, 1)
-    elif len(rolls) == 3:
+
+        ws = [0, 0]
+        for i in range(1, 4):
+            for j in range(1, 4):
+                for k in range(1, 4):
+                    w = find(seq1, seq2, i + j + k, next_turn)
+                    ws[0] += w[0]
+                    ws[1] += w[1]
+
+        return ws
+    else:
         next_turn = not p1
 
         if p1:
-            n1 = seq1 + [nx(seq1[-1], sum(rolls))]
+            n1 = seq1 + [nx(seq1[-1], roll)]
             k = str(n1) + '|' + str(seq2)
             if k in memo:
                 return memo[k]
-            r = find(n1, seq2, [], next_turn)
+            r = find(n1, seq2, -1, next_turn)
             memo[k] = r
         else:
-            n2 = seq2 + [nx(seq2[-1], sum(rolls))]
+            n2 = seq2 + [nx(seq2[-1], roll)]
             k = str(seq1) + '|' + str(n2)
             if k in memo:
                 return memo[k]
-            r = find(seq1, n2, [], next_turn)
+            r = find(seq1, n2, -1, next_turn)
             memo[k] = r
         #if r[0] > 1000000000000 or r[1] > 1000000000000:
         #   print('memo', k, r)
         return r
 
-    w1 = find(seq1, seq2, rolls + [1], next_turn)
-    w2 = find(seq1, seq2, rolls + [2], next_turn)
-    w3 = find(seq1, seq2, rolls + [3], next_turn)
-
-    return (w1[0] + w2[0] + w3[0], w1[1] + w2[1] + w3[1])
-
-r = find(seq1, seq2, [], True)
+r = find(seq1, seq2, -1, True)
 
 print('part2', max(r[0], r[1]))
