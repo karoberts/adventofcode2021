3292c26839d66a759eaeab6da6ed53aa6a21a29f
kroberts <cppwriter@yahoo.com>
2022-01-24 22:54:16 -0800

    3-2

--- 03.clj
+++ 03.clj
@@ -46,3 +46,51 @@
 
 (println "part1" (* r3a r3b))
 
+(defn count-bits-pos
+  ([lines pos] (count-bits-pos lines pos 0))
+  ([lines pos count]
+    (if (empty? lines)
+      count
+      (count-bits-pos
+        (rest lines)
+        pos
+        (+ count (nth (first lines) pos))))))
+
+(defn filter-by-bits [lines pos bitval]
+  (filter 
+    #(= (nth %1 pos) bitval)
+    lines))
+
+(defn most-common [bitcount ct]
+  (if (= (rem ct 2) 0)
+    (if (>= bitcount (quot ct 2)) 1 0)
+    (if (> bitcount (quot ct 2)) 1 0)))
+
+(defn least-common [bitcount ct]
+  (if (= (rem ct 2) 0)
+    (if (>= bitcount (quot ct 2)) 0 1)
+    (if (<= bitcount (quot ct 2)) 1 0)))
+
+(defn do-part-2 [lines pos len tgt]
+  (if (or (= pos (dec len)) (= 1 (count lines)))
+    (vec (first lines))
+    (do
+      (let [bitcount (count-bits-pos lines pos)
+            ct (count lines)
+            mostcommon (most-common bitcount ct)
+            leastcommon (least-common bitcount ct)]
+        (do-part-2
+          (filter-by-bits lines pos (if (= tgt 1) mostcommon leastcommon))
+          (inc pos)
+          len
+          tgt)))))
+  
+(time
+  (do
+    (def r3_2a (do-part-2 lines 0 (r1 1) 1))
+    (def r3_2a2 (compute-binary r3_2a))
+
+    (def r3_2b (do-part-2 lines 0 (r1 1) 0))
+    (def r3_2b2 (compute-binary r3_2b))))
+
+(println "part2" (* r3_2a2 r3_2b2))
