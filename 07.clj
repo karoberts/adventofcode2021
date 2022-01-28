92f4aa5e5d75e378f86d0748038d41c8d0b8444b
kroberts <cppwriter@yahoo.com>
2022-01-27 17:35:55 -0800

    day 7

---
+++ 07.clj
@@ -0,0 +1,39 @@
+(def lines (clojure.string/split-lines (slurp "7.txt")))
+
+(def crabs
+  (map
+    #(Integer/parseInt %)
+    (clojure.string/split (first lines) #",")))
+    
+;(def crabs [16 1 2 0 4 2 7 1 2 14])
+
+(defn abs [n] (max n (-' n)))
+
+(defn do-part1 [crabs]
+  (apply min
+    (let [mincrab (apply min crabs)
+          maxcrab (apply max crabs)]
+      (into []
+        (for [c (range mincrab (inc maxcrab))]
+          (->> crabs
+            (map #(abs (- %1 c)))
+            (reduce +)))))))
+
+(defn do-part2 [crabs]
+  (letfn [
+    (calc [i x]
+      (let [diff (abs (- x i))]
+        (as-> (inc diff) $ 
+          (* diff $)
+          (quot $ 2))))]
+    (apply min
+      (let [mincrab (apply min crabs)
+            maxcrab (apply max crabs)]
+        (into []
+          (for [c (range mincrab (inc maxcrab))]
+            (->> crabs
+              (map (partial calc c))
+              (reduce +))))))))
+
+(println "part1" (do-part1 crabs))
+(println "part2" (do-part2 crabs))
